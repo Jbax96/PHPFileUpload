d8c0b6aa0f95221dc68a14de88544881c66317f9
Jordan Baxter <JBaxter96@hotmail.co.uk>
2018-05-31 17:51:26 +0100

    Work in progress PHP file upload

---
+++ settings.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 31/05/2018
+ * Time: 15:59
+ * Class Settings holds the upload settings
+ */
+
+class settings
+{
+    static $password = "mypassword";
+    static $uploadFolder = "uploads/";
+}
+?>
